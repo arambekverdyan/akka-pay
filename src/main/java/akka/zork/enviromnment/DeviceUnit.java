2020117e015460b26c9cb2fd114dad3f9c42bfec
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2019-03-28 14:25:29 +0400

    Code formating

--- src/main/java/akka/zork/enviromnment/DeviceUnit.java
+++ src/main/java/akka/zork/enviromnment/DeviceUnit.java
@@ -2,12 +2,13 @@ package akka.zork.enviromnment;
 
 import java.util.Optional;
 
-class ReadTemperature {}
+class ReadTemperature {
+}
 
 class RespondTemperature {
-    final Optional<Double> value;
+  final Optional<Double> value;
 
-    public RespondTemperature(Optional<Double> value) {
-        this.value = value;
-    }
+  public RespondTemperature(Optional<Double> value) {
+    this.value = value;
+  }
 }
