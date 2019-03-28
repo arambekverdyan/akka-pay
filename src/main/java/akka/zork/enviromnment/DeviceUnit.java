850bbfbd859057033f8859b6424e00df43a1df1f
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2019-03-28 14:21:06 +0400

    Added message types for device unit interaction

---
+++ src/main/java/akka/zork/enviromnment/DeviceUnit.java
@@ -0,0 +1,13 @@
+package akka.zork.enviromnment;
+
+import java.util.Optional;
+
+class ReadTemperature {}
+
+class RespondTemperature {
+    final Optional<Double> value;
+
+    public RespondTemperature(Optional<Double> value) {
+        this.value = value;
+    }
+}
