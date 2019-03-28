5d6ac3b8ae679cb050290a4fa88b160030553d64
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2019-03-29 01:29:00 +0400

    Added request identifiers to messages

--- src/main/java/akka/zork/enviromnment/DeviceUnit.java
+++ src/main/java/akka/zork/enviromnment/DeviceUnit.java
@@ -3,12 +3,19 @@ package akka.zork.enviromnment;
 import java.util.Optional;
 
 class ReadTemperature {
+  final long requestId;
+
+  public ReadTemperature(long requestId) {
+    this.requestId = requestId;
+  }
 }
 
 class RespondTemperature {
+  final long requestId;
   final Optional<Double> value;
 
-  public RespondTemperature(Optional<Double> value) {
+  public RespondTemperature(long requestId, Optional<Double> value) {
+    this.requestId = requestId;
     this.value = value;
   }
 }
